deadb7dca35321ba1b805c5d7795d42bcee955a6
nataliaev <nataliiaevchenko@gmail.com>
2019-08-10 13:25:33 +0200

    Add new baseURL

--- src/actions.js
+++ src/actions.js
@@ -2,9 +2,7 @@ import request from 'superagent'
 
 export const ALL_IMAGES = 'ALL_IMAGES'
 
-const baseUrl = 'http://localhost:4000'
-
-//const baseUrl = 'https://mighty-sea-90292.herokuapp.com' || 'http://localhost:4000'
+const baseUrl = 'https://safe-sea-63608.herokuapp.com/' || 'http://localhost:4000'
 
 function allImages (payload) {
   return {
